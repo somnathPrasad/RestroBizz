d5e9029abbe3d5a86c08afa1a47dc42d4f4c86c3
Somnath Prasad <somnathprasad559@gmail.com>
2023-01-22 13:39:22 +0530

    prisma.ts file typescript error resolve

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,20 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+declare let global: { prisma: PrismaClient };
 
-let prisma: PrismaClient;
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more: 
+// https://pris.ly/d/help/next-js-best-practices
+
+let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient()
   }
-  prisma = global.prisma;
+  prisma = global.prisma
 }
-
-export default prisma;
+export default prisma
